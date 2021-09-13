4325ab12cec2627c676df7ac78526d2fc57d57cb
jenobpj <jenobgit@gmail.com>
2021-09-13 19:25:47 -0400

    tail added

---
+++ tail.js
@@ -0,0 +1,13 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+const tail = (array)=>{
+  return array.slice(1);
+};
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result,["Lighthouse","Labs"]); 
