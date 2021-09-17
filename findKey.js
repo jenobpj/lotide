d855d70d6000a3b46cff31717140da444ce09b19
jenobpj <jenobgit@gmail.com>
2021-09-16 21:17:41 -0400

    findKey added

---
+++ findKey.js
@@ -0,0 +1,30 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+
+const findKey=(object,cb)=>{
+  for(let obj in object){
+    if(cb(object[obj])){
+   return obj
+  }
+}
+return undefined
+
+}
+
+
+
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2),'noma')// => "noma"
+
+
