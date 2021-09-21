74f1e949118b21f213525d2b7ac7b4085823e139
jenobpj <jenobgit@gmail.com>
2021-09-21 15:00:14 -0400

    module updated

--- middle.js
+++ middle.js
@@ -1,26 +1,5 @@
-/* EqArray function */
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-};
-/*Assert Equal function */
-const assertArrayEqual=function(arr1,arr2){
-  const result=eqArrays(arr1,arr2)
-  if(result){
-    console.log(`✅ ✅ ✅  Assertion passed: ${arr1} === ${arr2}`);
-  }
-  else{
-    console.log(`❌ ❌ ❌  Assertion failed: ${arr1} !== ${arr2}`);
-  }
-};
+const eqArrays=require('./eqArray')
+const assertEqual=require('./assertEqual')
 /* Middle element finder */
 const middle=(array)=>{
   let middleArray=[];
@@ -40,4 +19,5 @@ const middle=(array)=>{
   
 
 }
-middle([1,2,3,4,5,5,6,7,8])
+
+module.exports=middle;
